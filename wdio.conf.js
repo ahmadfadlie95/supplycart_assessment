93278cac1df110fe09a18bb1b1ee1a74226ae670
fadlie.hadi <fadlie.hadi@silentmode.my>
2024-09-24 12:54:37 +0800

    retry ci again

--- wdio.conf.js
+++ wdio.conf.js
@@ -2,10 +2,10 @@ exports.config = {
     // ====================
     // BrowserStack Config
     // ====================
-    user: process.env.BROWSERSTACK_USERNAME,
-    key: process.env.BROWSERSTACK_ACCESS_KEY,
-    // user: 'ahmadfadliehadib_AzGhEB',
-    // key: '6FzybP8va7qjbxxNV68m',
+    // user: process.env.BROWSERSTACK_USERNAME,
+    // key: process.env.BROWSERSTACK_ACCESS_KEY,
+    user: 'ahmadfadliehadib_AzGhEB',
+    key: '6FzybP8va7qjbxxNV68m',
     browserstackLocal: true,
     //
     // ====================
